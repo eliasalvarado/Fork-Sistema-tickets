c539edf3a50ec773f05174633c35fc1ea40de820
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2026-03-16 10:55:19 -0600

    feat ✨ Átomo DropdownMenu

---
+++ src/components/client/molecules/DropdownMenu/DrowdownMenu.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { useState } from "react";
+import { DropdownMenu } from "./DropdownMenu";
+
+const meta = {
+  title: "Molecules/DropdownMenu",
+  component: DropdownMenu,
+  parameters: {
+    layout: "padded",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    items: {
+      control: "object",
+      description: "Lista de opciones del menú desplegable",
+    },
+    onSelect: {
+      action: "selected",
+      description: "Función que se ejecuta al seleccionar una opción",
+    },
+  },
+} satisfies Meta<typeof DropdownMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      { label: "Sistema Tickets", value: "option1" },
+      { label: "Sistema de Gestión", value: "option2" },
+      { label: "Sistema de Facturación", value: "option3" },
+    ],
+    onSelect: () => {},
+  },
+
+  render: (args) => {
+    const [selectedItem, setSelectedItem] = useState(args.items[0]);
+    const handleSelect = (item: any) => {
+      setSelectedItem(item);
+      args.onSelect(item);
+    };
+
+    return (
+      <>
+        <DropdownMenu {...args} items={args.items} onSelect={handleSelect} />
+        <p>Elemento seleccionado: {selectedItem.value}</p>
+      </>
+    );
+  }
+};
